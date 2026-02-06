2fd3dfe3f29b8a8fbd79685ff83791cd554048e9
Amit <iamit.kr.p@gmail.com>
2026-02-06 22:01:57 +0530

    vite-fix, vercel-fix

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
 import glsl from 'vite-plugin-glsl'
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default {
-    root: './',
-    publicDir: 'static/',
+    root: 'src/',
+    publicDir: '../static/',
     base: './',
     build:
     {
-        outDir: 'dist',
+        outDir: '../dist',
         emptyOutDir: true,
         rollupOptions: {
             input: {
-                main: 'src/index.html',
-                admin: 'src/admin.html'
+                main: resolve(__dirname, 'src/index.html'),
+                admin: resolve(__dirname, 'src/admin.html')
             }
         }
     },
